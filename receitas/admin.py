cf937fbbec32d0d8192beb35f54957f94290c03e
Pedro Henrique Fernandes <pedrohcf3.14@gmail.com>
2021-06-04 23:50:25 -0300

    adicionado exibição de fotos nas receitas

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
     list_display =('id', 'nome_receita', 'pessoa', 'categoria', 'publicada')
-    list_editable = ('publicada','categoria')
+    list_editable = ('publicada',)
     list_display_links=('id', 'nome_receita', 'pessoa')
     list_filter=('categoria', 'pessoa', 'publicada')
     search_fields=('nome_receita', 'categoria', 'pessoa__nome')
